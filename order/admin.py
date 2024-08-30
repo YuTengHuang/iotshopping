82903aeaf8588efaf158ef737dabd8ac9fb43660
YuTen <142889670+YuTengHuang@users.noreply.github.com>
2024-08-30 09:50:39 +0800

    Update admin.py

--- order/admin.py
+++ order/admin.py
@@ -2,13 +2,11 @@ from django.contrib import admin
 from .models import Order
 # Register your models here.
 
-## 此為自訂Django管理頁面的table顯示欄位
+
 class OderManager(admin.ModelAdmin):
     list_display = ( "order_trackid", "username", "get_order_status_display",)
-    # list_display_links =("username",)   將username指定為點擊連結, 而不是上述第一個
 
     def username(self, obj):
         return obj.uid.member_username
-
 
 admin.site.register(Order, OderManager)
