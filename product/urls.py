494dc84c7dc1da4f5e89144d24c0cf19e0cb0e52
YuTen <142889670+YuTengHuang@users.noreply.github.com>
2024-08-30 09:49:17 +0800

    Update urls.py

--- product/urls.py
+++ product/urls.py
@@ -3,8 +3,7 @@ from product import views as pv
 
 
 urlpatterns = [
-    # path('product/', pv.LateProductList.as_view()), # 沒用裝飾器用法
-    path('product/', pv.late_product_list),           # 裝飾器用法
+    path('product/', pv.late_product_list),          
     path('productsimpleinfo/', pv.get_product_simple_info),
     path('productpageinfo/', pv.get_product_page_info)
 ]
